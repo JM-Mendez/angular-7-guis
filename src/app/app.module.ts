41aa4ad03f6a5af1981e178c1adaeba62163cf22
John Mendez <john.manuel.mendez@gmail.com>
2020-10-17 16:59:00 -0400

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TempWidgetComponent } from './temp-widget/temp-widget.component';
+import { CelsiusComponent } from './temp-widget/celsius/celsius.component';
+import { FahrenheitComponent } from './temp-widget/fahrenheit/fahrenheit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TempWidgetComponent,
+    CelsiusComponent,
+    FahrenheitComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/temp-widget/temp-widget.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TempWidgetComponent } from './temp-widget.component';
+
+describe('TempWidgetComponent', () => {
+  let component: TempWidgetComponent;
+  let fixture: ComponentFixture<TempWidgetComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TempWidgetComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TempWidgetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
