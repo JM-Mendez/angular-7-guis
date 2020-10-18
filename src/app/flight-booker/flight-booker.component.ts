1864e9f4c76132302708a0dfd8f8032e5deed785
John Mendez <john.manuel.mendez@gmail.com>
2020-10-17 21:18:07 -0400

    add combobox

--- src/app/flight-booker/flight-booker.component.ts
+++ src/app/flight-booker/flight-booker.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-flight-booker',
   templateUrl: './flight-booker.component.html',
-  styleUrls: ['./flight-booker.component.css']
+  styleUrls: ['./flight-booker.component.css'],
 })
 export class FlightBookerComponent implements OnInit {
+  flightType: 'one-way flight' | 'return flight' = 'one-way flight';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
